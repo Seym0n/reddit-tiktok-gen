a2d6a4800d817accd913b41b68b9b434624e7341
Simon <simon.s6@web.de>
2024-08-21 02:03:58 +0200

    feat: Update ElevenLabs model ID to "eleven_turbo_v2_5" for improved TTS performance

--- app/utils/elevenlabs.py
+++ app/utils/elevenlabs.py
@@ -29,7 +29,7 @@ class ElevenLabs(TTS):
         # todo review model and settings
         data = {
             "text": text,
-            "model_id": "eleven_monolingual_v1",
+            "model_id": "eleven_turbo_v2_5",
             "voice_settings": {"stability": 0.5, "similarity_boost": 0.5},
         }
 
